5cbb78d72fc8c56f726702f7913ca52c0535e8a5
HDS <jonathan.canales.hds@gmail.com>
2021-04-16 19:57:46 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Aprendiendo React desde 0 a Experto</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/HDS2904/aprendiendo_react"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Repositorio en GitHub con los temas divididos en als diferentes ramas
         </a>
       </header>
     </div>
