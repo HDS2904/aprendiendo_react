567d41dbaa1b329ebd489b4579d926da8615343a
HDS <jonathan.canales.hds@gmail.com>
2021-04-18 03:58:15 -0500

    DefauldProps

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -2,12 +2,13 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 // Funsional components
-const PrimeraApp = ( { saludo } ) => {
+const PrimeraApp = ( { saludo, subtitulo } ) => {
     
     return (
         <>
             <h1>PropTypes</h1>
             <h3> { saludo } </h3>
+            <h3> { subtitulo } </h3>
         </>
     );
 }
@@ -15,6 +16,11 @@ const PrimeraApp = ( { saludo } ) => {
 // Código para obligar el envio de parametros "required"
 PrimeraApp.propTypes = {
     saludo: PropTypes.string.isRequired
+}
+
+// Definición de contedido de los props por defecto
+PrimeraApp.defaultProps = {
+    subtitulo: 'Un subtitulo por defecto'
 }
 
 export default PrimeraApp;
