40ccd98874eeae0101ff6db3b3fcb5ca1b3e2fe7
HDS <jonathan.canales.hds@gmail.com>
2021-04-18 03:27:07 -0500

    Comunicación entre componentes

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -2,24 +2,12 @@ import React, { Fragment } from 'react';
 
 
 // Funsional components
-const PrimeraApp = () => {
+const PrimeraApp = ( { saludo = 'Sin contenido' } ) => {
     
-    const cadena = "Hola mundo";
-    const boleano = true;
-    const arreglo = [1,2,3,4,5,6];
-    const objeto = {
-        name: 'Carlos',
-        age: 15,
-    };
-
     return (
         <>
-            <h1>Variables en React</h1>
-            <h3> { cadena } </h3>
-            <h3> { boleano } </h3>
-            <h3> { arreglo } </h3>
-            {/* <h3> { objeto } </h3> */}  
-            <pre> { JSON.stringify( objeto, null, 3 ) } </pre> 
+            <h1>Extrayendo datos de props</h1>
+            <h3> { saludo } </h3>
         </>
     );
 }
