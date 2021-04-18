2897cf42bdf1e5733ad0b5448e394a49d03835e0
HDS <jonathan.canales.hds@gmail.com>
2021-04-18 02:48:57 -0500

    Retorno de elementos al componente

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+
 
 // Funsional components
 const PrimeraApp = () => {
-    return <h1>Hola mundo react, conquistare todo</h1>;
+    
+    // PRIMERA FORMA
+    // return (
+    //     <div>
+    //         <h1>Hola mundo react, dominare todo</h1>
+    //         <p>Mas contenidooooo</p>
+    //     </div>
+    // );
+
+    // SEGUNDA FORMA
+    // return (
+    //     <Fragment>
+    //         <h1>Hola mundo react, dominare todo</h1>
+    //         <p>Mas contenidooooo</p>
+    //     </Fragment>
+    // );
+
+    // CUARTA FORMA (Recomendada)
+    return (
+        <>
+            <h1>Hola mundo react, dominare todo</h1>
+            <p>Mas contenidooooo</p>
+        </>
+    );
 }
 
 export default PrimeraApp;
