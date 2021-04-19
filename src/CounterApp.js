cfcd293e979f9d6adbd417a248618f247e7bf7d8
HDS <jonathan.canales.hds@gmail.com>
2021-04-18 21:36:05 -0500

    Segunda tarea resuelta

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -4,19 +4,22 @@ import PropTypes from 'prop-types';
 const CounterApp = ( { value } ) => {
 
     // Uso de Hooks
-    const [ counter, setCounter ] = useState( 0 );
+    const [ counter, setCounter ] = useState( value );
 
     //Llamado de la funcion obtenida al desestructurar useState
-    const handleAdd = ( e ) => {
-        setCounter(counter + 1);
-        // setCounter( ( c ) => c + 1 )
-    }
+    const handleAdd = (  ) => setCounter(counter + 1)
+
+    const handlePop = (  ) => setCounter(counter - 1)
+
+    const handleReset = (  ) => setCounter( value )
 
     return (
         <>
             <h1>CounterApp</h1>
             <h2> { counter } </h2>
             <button onClick={ handleAdd }> +1 </button>
+            <button onClick={ handleReset }> reset </button>
+            <button onClick={ handlePop }> -1 </button>
         </>
     );
 }
