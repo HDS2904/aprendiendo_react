c023ffad14de392da07fe983851f02a3521b54a7
HDS <jonathan.canales.hds@gmail.com>
2021-05-18 14:29:31 -0500

    Simulador de cambios en un input

--- src/tests/components/CategoryAdd.test.js
+++ src/tests/components/CategoryAdd.test.js
@@ -4,11 +4,17 @@ import { CategoryAdd } from "../../components/CategoryAdd"
 describe('Pruebas del componente CategoryAdd.js', () => {
 
   const setCategory = () => {}
+  const wrapper = shallow( <CategoryAdd setCategory={setCategory} /> )
 
   test('Retorna la estructura HTML correcta del compoente', () => {
-    const wrapper = shallow( <CategoryAdd setCategory={setCategory} /> )
     expect( wrapper ).toMatchSnapshot()
   })
-  
+
+  test('Debe de cambiar la caja de texto', () => {
+    const tagInput = wrapper.find('input')
+    const textValue = 'Mi linea de texto'
+    tagInput.simulate('change', { target: { value: textValue } })
+    expect( wrapper.find('p').text().trim() ).toBe( textValue )
+  })
 
 })
