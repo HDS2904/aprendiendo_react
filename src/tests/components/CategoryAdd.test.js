c5d2cf47df3d250db1463fd9d200f57b794f7e3b
HDS <jonathan.canales.hds@gmail.com>
2021-05-27 10:27:42 -0500

    simular submit, llamado de función y limpiado del input value

--- src/tests/components/CategoryAdd.test.js
+++ src/tests/components/CategoryAdd.test.js
@@ -1,14 +1,15 @@
+import '@testing-library/jest-dom'
 import { shallow } from "enzyme"
 import { CategoryAdd } from "../../components/CategoryAdd"
 
 describe('Pruebas del componente CategoryAdd.js', () => {
 
-  const setCategory = jest.fn()
-  let wrapper = shallow( <CategoryAdd setCategory={setCategory} /> )
+  const setCategories = jest.fn()
+  let wrapper = shallow( <CategoryAdd setCategories={setCategories} /> )
   
-  beforeEach( () => {
-    jest.clearAllMocks()
-    wrapper = shallow( <CategoryAdd setCategory={setCategory} /> )
+  beforeEach( () => { //función del ciclo de vida del componente
+    jest.clearAllMocks()  //Limpiesa del componente ante simulaciones
+    wrapper = shallow( <CategoryAdd setCategories={setCategories} /> ) // inicialización del componente
   }) 
 
   test('Retorna la estructura HTML correcta del compoente', () => {
@@ -18,13 +19,31 @@ describe('Pruebas del componente CategoryAdd.js', () => {
   test('Debe de cambiar la caja de texto', () => {
     const tagInput = wrapper.find('input')
     const textValue = 'Mi linea de texto'
-    tagInput.simulate('change', { target: { value: textValue } })
+    tagInput.simulate('change', { target: { value: textValue } }) //simula cambio en la etiqueta input
     expect( wrapper.find('p').text().trim() ).toBe( textValue )
   })
 
   test('No debe ejecutar la función submit', () => {
-    wrapper.find('form').simulate('submit', { preventDefault(){} })
-    expect( setCategory ).not.toHaveBeenCalled()
+    wrapper.find('form').simulate('submit', { preventDefault(){} }) // simulación de submit
+    expect( setCategories ).not.toHaveBeenCalled() //evalua si la función setCategories no se ejecuto
   })
-  
+
+  test('Llamar el setCategories y limpiar la caja de texto', () => {
+    const value = 'Nuevo texto'
+    // Simular el camvio del atributo value del input
+    wrapper.find('input').simulate('change',{ target: { value } })
+    // Simular el submit del formulario
+    wrapper.find('form').simulate('submit',{ preventDefault(){} })
+    // Verificar si fue llamada la función setCategories
+    expect( setCategories ).toHaveBeenCalled()
+    // expect( setCategories ).toHaveBeenCalledTimes(2) // Evalua si la función se llamo 2 veces
+    // expect( setCategories ).toHaveBeenCalledWith() // Evalua si la función se llamo con otra función (y no enteros o numeros por arguemnto)
+
+    expect( setCategories ).toHaveBeenCalled()
+
+    // Evaluar si el input esta vacio luego del submit
+    expect( wrapper.find('input').prop('value') ).toBe( '' )
+
+  })
+
 })
