e5bce7f7014502d30d8f278ff1994fd8f3ff7977
HDS <jonathan.canales.hds@gmail.com>
2021-05-31 10:59:06 -0500

    Custom Hooks waitForNextUpdate

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -3,12 +3,22 @@ import { useFetchGifs } from '../../hooks/useFetchGifs'
 
 describe('Prueba del hook del proyecto', () => {
   
-  test('debe verificar el contenido inicial de la variable en hook', () => {
-    const { result } = renderHook( () => useFetchGifs('one piece') )
+  test('debe verificar el contenido inicial de la variable en hook', async () => {
+    const { result, waitForNextUpdate } = renderHook( () => useFetchGifs('one piece') )
     const { data, loading } = result.current
+    await waitForNextUpdate()
+
     expect( data ).toEqual( [] )
-    expect( loading ).toBe( true)
+    expect( loading ).toBe( true )
+  })
+
+  test('Evalua el numero de elementos cargados en el componente', async () => {
+    const { result, waitForNextUpdate } = renderHook( () => useFetchGifs('one piece') )
+    await waitForNextUpdate()
+    const { data, loading } = result.current
+
+    expect( data.length ).toBe( 10 )
+    expect( loading ).toBe( false )
   })
   
-
 })
