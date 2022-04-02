4f3c56278a1564680ae2a4d502af25b709beaada
Jonathan Canales Chavez <jonathan.canales.hds@gmail.com>
2022-04-02 18:55:19 -0500

    useEffect: precauciones

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,15 +1,26 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
+	const [coords, setCoords] = useState({x:0,y:0});
+	const { x, y } = coords;
+
+	const mouseMove = (e) => {
+		const coords = { x: e.x, y: e.y };
+		setCoords(coords);
+	}
+
   useEffect(() => {
-    console.log('Se aperturo el hook');
+		window.addEventListener('mousemove',mouseMove);
     return () => {
-      console.log('Se cerro el hook');
+      window.removeEventListener('mousemove',mouseMove)
     }
   }, [])
   return (
     <div>
       <h2>Hola Jonathan!</h2>
+			<p>
+				x: {x}, y: {y}
+			</p>
     </div>
   )
 }
