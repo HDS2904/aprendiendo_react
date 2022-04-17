778741b73b7cfdd728bcc9e31587586a350d7e19
Jonathan Canales Chavez <jonathan.canales.hds@gmail.com>
2022-04-17 14:29:18 -0500

    CreateContext y useContext

--- src/components/09-useContex/MainApp.js
+++ src/components/09-useContex/MainApp.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
+
 
 export const MainApp = () => {
-	return <AppRouter/>
+	const user = {
+		id: 120,
+		name: 'Jonathan',
+		email: 'jonathan.canales.hds@gmail.com',
+	}
+	return (
+		<UserContext.Provider value={user} >
+			<AppRouter/>
+		</UserContext.Provider>
+	)
 }
