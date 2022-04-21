04906ac76b29c79ca82faca2f53cf40ef8ce18e1
Jonathan Canales Chavez <jonathan.canales.hds@gmail.com>
2022-04-21 09:59:06 -0500

    Ejecutar funciones del customHook dentro de las pruebas

--- src/tests/hooks/useCounter.test.js
+++ src/tests/hooks/useCounter.test.js
@@ -1,17 +1,71 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react-hooks';
 import { useCounter } from '../../hooks/useCounter';
 
 describe('Prueba en useCounter', () => {
 
 	test('Debe retornar por defecto', () => {
 
-		const { result } = renderHook( () => useCounter() );
+		const { result } = renderHook( () => useCounter(10) );
 		
-		expect( result.current.state ).toBe( 0 );
+		expect( result.current.state ).toBe( 10 );
 		expect( typeof result.current.decrement ).toBe( 'function' );
 		expect( typeof result.current.increment ).toBe( 'function' );
 		expect( typeof result.current.reset ).toBe( 'function' );
-
-
+		
 	})
+	
+	test('Debe incrementar el counter en 1', () => {
+		const { result } = renderHook( () => useCounter(100) );
+
+		// Incrementando el contador
+		act( () => {
+			result.current.increment();
+		})
+		expect( result.current.state ).toBe( 101 );
+	})
+
+	test('Debe decrementar el counter en 1', () => {
+		const { result } = renderHook( () => useCounter(100) );
+
+		// Decrementando el contador
+		act( () => {
+			result.current.decrement();
+		})
+		expect( result.current.state ).toBe( 99 );
+	})
+
+	test('Debe resetear el valor por default', () => {
+		const { result } = renderHook( () => useCounter(100) );
+
+		// Incrementando el contador
+		act( () => {
+			result.current.increment();
+		})
+		expect( result.current.state ).toBe( 101 );
+		
+		// Reseteando el contador
+		act( () => {
+			result.current.reset();
+		})
+		expect( result.current.state ).toBe( 100 );
+	})
+
+	test('Incrementando el contador dos veces', () => {
+		const { result } = renderHook( () => useCounter(100) );
+
+		// Incrementando el contador dos veces
+		act( () => {
+			result.current.increment();
+		})
+		act( () => {
+			result.current.increment();
+		})
+		expect( result.current.state ).toBe( 102 );
+	})
+
 })
+
+// ACT
+// Dentro del act, cada funcipon se ejecutará de una misma base bloqueada
+// del hook, por lo que no guarda los cambios ejecutados por mas de una
+// función
